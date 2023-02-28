d0ffc89ef3639e0a7914f682dd5a45430ccf6a07
theonewhoknew <theonewhoknew@protonmail.com>
2023-02-28 09:48:37 +0100

    Make loop in user input so if answer wrong it's asked again

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -18,8 +18,10 @@ function getComputerChoice() {
 let choice 
 
 function getUserChoice() {
+    for (choice; choice != "rock" || choice != "paper" || choice != "scissors";) {
     choice = prompt("Rock, Paper or Scissors?")
     choice = choice.toLowerCase();
+
     if (choice === "rock") {
         return "rock"
     }
@@ -34,6 +36,7 @@ function getUserChoice() {
             alert("answer not valid")
         }
     }
+    }
     
 function game(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
@@ -42,33 +45,61 @@ function game(playerSelection, computerSelection){
     else {
         if (playerSelection === "rock" && computerSelection === "scissors") {
             alert("You win! Rock beats Scissors.")
+            result = "user-wins"
         }
 
         else if (playerSelection === "scissors" && computerSelection === "rock") {
             alert("You lose! Rock beats Scissors.")
+            result = "cpu-wins"
         }
 
         else if (playerSelection === "paper" && computerSelection === "rock") {
             alert("You win! Paper beats Rock.")
+            result = "user-wins"
         }
 
         else if (playerSelection === "rock" && computerSelection === "paper") {
             alert("You lose! Paper beats Rock.")
+            result = "cpu-wins"
         }
 
         else if (playerSelection === "scissors" && computerSelection === "paper") {
             alert("You win! Scissors beats paper.")
+            result = "user-wins"
         }
 
         else if (playerSelection === "paper" && computerSelection === "scissors") {
             alert("You lose! Scissors beats paper.")
+            result = "cpu-wins"
         }
 
         
     }
 }
+
+let result
+let userScore = 0
+let cpuScore = 0
+
 for(let i = 0; i < 5; i++) {
  game(getUserChoice(),getComputerChoice())
+ if (result === "user-wins") {
+    console.log("User Score "+(++userScore))
+ }
+ else if (result === "cpu-wins") {
+    console.log("CPU Score "+(++cpuScore))
+ }
+
+ else {}
 }
 
+if (userScore > cpuScore) {
+    console.log("You won the game!")
+}
+    else if (userScore === cpuScore) {
+    console.log ("There is no winner!")
+}
+    else {
+    console.log("You lost the game!")
+}
 
