3115b9207d5c4ce05ec92cf888ee34a56d9cee60
theonewhoknew <theonewhoknew@protonmail.com>
2023-03-03 11:18:05 +0100

    make correct scores and add color hierarchy to determine winner of round, and final text to declare winner of game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,26 +13,50 @@ rockbutton.innerHTML = "ROCK"
 paperbutton.innerHTML = "PAPER"
 scissorsbutton.innerHTML = "SCISSORS"
 
-const scorecontainer = document.createElement("div") 
-document.body.appendChild(scorecontainer)
+const scoreContainer = document.createElement("div") 
+document.body.appendChild(scoreContainer)
 const userScore = document.createElement("div")
-scorecontainer.appendChild(userScore)
+scoreContainer.appendChild(userScore)
 const computerScore = document.createElement("div")
-scorecontainer.appendChild(computerScore)
+scoreContainer.appendChild(computerScore)
 const rounds = document.createElement("div")
-scorecontainer.insertBefore(rounds, computerScore)
-scorecontainer.classList.add("scorecontainer")
+scoreContainer.insertBefore(rounds, computerScore)
+scoreContainer.classList.add("scorecontainer")
 const resultcontainer = document.createElement("div")
 document.body.appendChild(resultcontainer)
 resultcontainer.classList.add("resultcontainer")
 
+const choiceContainer = document.createElement ("div")
+document.body.insertBefore(choiceContainer, scoreContainer)
+choiceContainer.style.display = "flex"
+choiceContainer.style.justifyContent = "space-between"
+choiceContainer.style.padding = "80px"
+const userChoice = document.createElement ("div")
+choiceContainer.appendChild (userChoice)
+userChoice.innerHTML = ""
+const verdict = document.createElement ("div")
+choiceContainer.appendChild(verdict)
+const cpuChoice = document.createElement ("div")
+choiceContainer.appendChild (cpuChoice)
+cpuChoice.innerHTML = ""
+
+
+
+const resetcontainer = document.createElement ("div")
+document.body.appendChild(resetcontainer)
+const resetbutton = document.createElement("button")
+resetcontainer.appendChild(resetbutton)
+resetcontainer.style.display = "flex"
+resetcontainer.style.justifyContent = "flex-end"
+resetbutton.innerHTML = "RESET"
+resetbutton.style.display = "none"
 
 
 
 
 
 function showResult (text) {
-    resultcontainer.innerHTML = text
+    verdict.innerHTML = text
 }
 
 let userScoreNum = 0
@@ -52,43 +76,52 @@ let result
 function getComputerChoice() {
     let number = Math.random();
     if ( number < 0.33) {
-        computerSelection = "rock"
+        computerSelection = "Rock"
     }
     else if (number > 0.33 && number < 0.66) {
-        computerSelection = "paper"
+        computerSelection = "Paper"
     }
     else {
-        computerSelection = "scissors"
+        computerSelection = "Scissors"
     }
 }
 
 
 
 function getRock() {
-    playerSelection = "rock";
+    playerSelection = "Rock";
     getComputerChoice();
 }
 function getPaper() {
-    playerSelection = "paper"
+    playerSelection = "Paper"
     getComputerChoice();
 }
 function getScissors() {
-    playerSelection = "scissors"
+    playerSelection = "Scissors"
     getComputerChoice();
 }
 
 function oneRound(playerSelection, computerSelection){
+    userChoice.style.color = "black"
+    cpuChoice.style.color = "black"
+
   
     if (playerSelection === computerSelection){
-        alert("Draw!");
+        userChoice.innerHTML = `${playerSelection}`;
+        userChoice.style.color = "orange"
+        cpuChoice.innerHTML = `${computerSelection}`;
+        cpuChoice.style.color = "orange"
         roundsPlayed += 1;
         rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
     }
     
         else {
                 
-            if (playerSelection === "rock" && computerSelection === "scissors") {
-                alert("You win! Rock beats Scissors.");
+            if (playerSelection === "Rock" && computerSelection === "Scissors") {
+                userChoice.innerHTML = "Rock";
+                userChoice.style.color = "green"
+                cpuChoice.innerHTML = "Scissors";
+                cpuChoice.style.color = "red"
                 roundsPlayed += 1;
                 rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
                 userScoreNum += 1;
@@ -97,8 +130,12 @@ function oneRound(playerSelection, computerSelection){
                     
             }
 
-            else if (playerSelection === "scissors" && computerSelection === "rock") {
-                alert("You lose! Rock beats Scissors.");
+            else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+                
+                userChoice.innerHTML = "Scissors";
+                userChoice.style.color = "red"
+                cpuChoice.innerHTML = "Rock";
+                cpuChoice.style.color = "green"
                 roundsPlayed += 1;
                 rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
                 computerScoreNum += 1;
@@ -106,8 +143,12 @@ function oneRound(playerSelection, computerSelection){
                 
             }
 
-            else if (playerSelection === "paper" && computerSelection === "rock") {
-                alert("You win! Paper beats Rock.");
+            else if (playerSelection === "Paper" && computerSelection === "Rock") {
+               
+                userChoice.innerHTML = "Paper";
+                userChoice.style.color = "green"
+                cpuChoice.innerHTML = "Rock";
+                cpuChoice.style.color = "red"
                 roundsPlayed += 1;
                 rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
                 userScoreNum += 1;
@@ -116,24 +157,36 @@ function oneRound(playerSelection, computerSelection){
                     
             }
 
-            else if (playerSelection === "rock" && computerSelection === "paper") {
-                alert("You lose! Paper beats Rock.");
+            else if (playerSelection === "Rock" && computerSelection === "Paper") {
+               
+                userChoice.innerHTML = "Rock";
+                userChoice.style.color = "red"
+                cpuChoice.innerHTML = "Paper";
+                cpuChoice.style.color = "green"
                 roundsPlayed += 1;
                 rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
                 computerScoreNum += 1;
                 computerScore.innerHTML = `CPU SCORE: ${computerScoreNum}`;
             }
 
-            else if (playerSelection === "scissors" && computerSelection === "paper") {
-                alert("You win! Scissors beats paper.");
+            else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+               
+                userChoice.innerHTML = "Scissors";
+                userChoice.style.color = "green"
+                cpuChoice.innerHTML = "Paper";
+                cpuChoice.style.color = "red"
                 roundsPlayed += 1;
                 rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
                 userScoreNum += 1;
                 userScore.innerHTML = `USER SCORE: ${userScoreNum}`;
             }
 
-            else if (playerSelection === "paper" && computerSelection === "scissors") {
-                alert("You lose! Scissors beats paper.");
+            else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+                
+                userChoice.innerHTML = "Paper";
+                userChoice.style.color = "red"
+                cpuChoice.innerHTML = "Scissors";
+                cpuChoice.style.color = "green"
                 roundsPlayed += 1;
                 rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
                 computerScoreNum += 1;
@@ -141,19 +194,36 @@ function oneRound(playerSelection, computerSelection){
             }      
         }
     if (roundsPlayed === 5) {
+    
+    
         if (userScoreNum > computerScoreNum) {
-            showResult("You win!")
+            showResult("You win! The Game is over!")
         }
 
         else if (userScoreNum === computerScoreNum) {
-            showResult("It's a draw!")
+            showResult("It's a draw! The Game is over!")
         }
 
         else {
-            showResult("You lose!")
+            showResult("You lose! The Game is Over!");
         } 
 
+    resetbutton.style.display = "initial"
     }
+}
+
+function resetGame () {
+    roundsPlayed = 0;
+    computerScoreNum = 0;
+    userScoreNum = 0;
+    rounds.innerHTML = `ROUNDS PLAYED: ${roundsPlayed}`;
+    userScore.innerHTML = `USER SCORE: ${userScoreNum}`;
+    computerScore.innerHTML = `CPU SCORE: ${computerScoreNum}`;
+    resetbutton.style.display = "none"
+    resultcontainer.innerHTML = ""
+    verdict.innerHTML = ""
+    cpuChoice.innerHTML = ""
+    userChoice.innerHTML = ""
 }
 
 rockbutton.addEventListener("click", () => {
@@ -173,6 +243,11 @@ scissorsbutton.addEventListener("click", () => {
     oneRound(playerSelection,computerSelection)
     console.log(`User:${playerSelection}; CPU:${computerSelection}`)
 });
+
+resetbutton.addEventListener("click", () => {
+    resetGame();
+
+})
 
 
 
